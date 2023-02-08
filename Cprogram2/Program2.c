0a2215e67843a93b9db963f2d84f558894b39990
William Laughlin <bill.laughlin@ku.edu>
2023-02-08 17:16:39 -0600

    Commit of Program2_Laughlin

--- Cprogram2/Program2.c
+++ Cprogram2/Program2.c
@@ -4,22 +4,41 @@
  *  Created on: Feb 8, 2023
  *      Author: w170l027
  */
-#include <stdio>
+#include <stdio.h>
 
 void combinations(int n)
 {
-	int TD;
-	int TDFG;
-	int TD2pt;
-	int FG;
-	int safety;
-	int ways = 0;
-
-	printf("Possible combinations of scoring plays:\n");
-	for(int i = 0; i < ways; i++)
+	if(n == 0 || n == 1)
 	{
-
-		printf("%d TD, %d TD + FG, %d TD + 2pt, %d 3pt FG, %d Safety", TD, TDFG, TD2pt, FG, safety);
+		return;
+	}
+	
+	int total;
+	printf("Possible combinations of scoring plays:\n");
+	for(int i = 0; i <= n/8; i++)
+	{
+		for(int j = 0; j <= n/7; j++)
+		{
+			for(int k = 0; k <= n/6; k++)
+			{
+				for(int l = 0; l <= n/3; l++)
+				{
+					for(int m = 0; m <= n/2; m++)
+					{
+						total = (i * 8) + (j * 7) + (k * 6) + (l * 3) + (m * 2);
+						if(total == n)
+						{
+							printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety", i, j, k, l, m);
+							printf("\n");
+						}
+						else if(total > n)
+						{
+							break;
+						}
+					}
+				}
+			}
+		}
 	}
 }
 
